5f3e16be472178d73d977a2623f88c1072df5d4f
Rizky Eventa Bahtiar <rizkyventa00@gmail.com>
2019-04-21 18:41:47 +0700

    first

---
+++ app/perpustakaan.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class perpustakaan extends Model
+{
+    //
+    protected $fillable = [
+        'judul',
+        'penerbit',
+        'tahun_terbit',
+        'pengarang'
+
+    ];
+}
